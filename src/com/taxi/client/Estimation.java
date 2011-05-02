8a1d376b1068375cac33d62e671745f0f0c575ae
Clément BIZEAU <clement.bizeau4@gmail.com>
2011-05-02 20:53:06 +0200

    amélioration de l'estimation de la course
    Signed-off-by: Clément BIZEAU <clement.bizeau4@gmail.com>

--- src/com/taxi/client/Estimation.java
+++ src/com/taxi/client/Estimation.java
@@ -6,17 +6,15 @@ import gmaps.DirectionNotFoundException;
 import gmaps.DirectionZeroResultsException;
 import gmaps.GmapsDirection;
 import gmaps.OverQueryLimitException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import rest_client.ConnectionException;
 import rest_client.HttpUrlException;
 import rest_client.ParamsException;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
+import android.widget.TextView;
 import client_request.ClientRequest;
 import core.TrajetInfo;
 import core.localisation.GeoPoint;
@@ -28,12 +26,12 @@ public class Estimation extends Activity {
 	 * 
 	 * @author Clement Bizeau & Yves Szymezak & Matthieu Bertrand
 	 */
-	private final static String server_addr = "http://88.184.190.42:8080";
-	private ListView ListView;
+	private final static String SERVER_ADDR = "http://88.184.190.42:8080";
+	private static final int UPDATE_UI = 0;
+	private static final int UPDATE_TIME = 120000;
 	private int idTaxi;
 	private SharedData data;
-	private ClientRequest req;
-	private HashMap<String, String> map;
+	private Handler handlerTimer = new Handler();
 
 	/** Called when the activity is first created. */
 	@Override
@@ -43,75 +41,76 @@ public class Estimation extends Activity {
 		data = (SharedData) getApplication();
 		Bundle b = getIntent().getExtras();
 		idTaxi = b.getInt("idTaxi");
-		Log.i("taxi", "idTaxi : " + idTaxi);
-		ListView = (ListView) findViewById(R.id.listview);
-		ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
-		map = new HashMap<String, String>();
-		map.put("titre", "Temps d'arrivée du taxi estimé");
-		req = new ClientRequest(server_addr);
+		
+		TextView tempsCourse = (TextView) findViewById(R.id.EstimationTempsTrajet);
+		TextView prix = (TextView) findViewById(R.id.EstimationPrix);
+		TrajetInfo infos;
 		try {
-			GeoPoint posTaxi;
-			posTaxi = req.getPosTaxi(idTaxi);
-			try {
-				map.put("description",
-						GmapsDirection.getTrajetInfo(posTaxi, data.position).temps);
-			} catch(OverQueryLimitException e) {
-				e.printStackTrace();
-			}
-			map.put("img", String.valueOf(R.drawable.taxi));
-			listItem.add(map);
-			map = new HashMap<String, String>();
-			map.put("titre", "Temps d'arrivée à destination");
-			TrajetInfo infos;
-			infos = GmapsDirection.getTrajetInfo(data.usrdestination,
-					data.position);
-			map.put("description", infos.temps);
-			map.put("img", String.valueOf(R.drawable.arrivee));
-			listItem.add(map);
-			map = new HashMap<String, String>();
-			map.put("titre", "Estimation du prix de la course");
-			Log.i("taxi", "dist : " + infos.distance);
-			map.put("description",
-					EstimationPrix.EstimPrix(infos.distanceValue));
-			map.put("img", String.valueOf(R.drawable.euro));
-			listItem.add(map);
-		} catch(ConnectionException e) {
-			Toast.makeText(this, "Connexion perdue", Toast.LENGTH_SHORT).show();
-		} catch(DirectionNotFoundException e1) {
-			e1.printStackTrace();
-		} catch(DirectionInvalidRequestException e1) {
-			e1.printStackTrace();
-		} catch(DirectionException e1) {
-			e1.printStackTrace();
-		} catch(DirectionZeroResultsException e1) {
-			e1.printStackTrace();
-		} catch(ParamsException e) {
+			infos = GmapsDirection.getTrajetInfo(
+					data.usrdestination, data.position);
+			tempsCourse.setText(infos.temps);
+			prix.setText(EstimationPrix.EstimPrix(infos.distanceValue));
+		} catch(DirectionNotFoundException e) {
 			e.printStackTrace();
-		} catch(HttpUrlException e) {
+		} catch(DirectionInvalidRequestException e) {
+			e.printStackTrace();
+		} catch(DirectionException e) {
+			e.printStackTrace();
+		} catch(DirectionZeroResultsException e) {
 			e.printStackTrace();
 		} catch(OverQueryLimitException e) {
+			tempsCourse.setText("inconnue");
+			prix.setText("inconnue");
 			e.printStackTrace();
 		}
-		SimpleAdapter mSchedule = new SimpleAdapter(getBaseContext(), listItem,
-				R.layout.item, new String[] { "img", "titre", "description" },
-				new int[] { R.id.img, R.id.titre, R.id.description });
-		ListView.setAdapter(mSchedule);
-		/*
-		 * final Timer timLocIpdate = new Timer(); timLocIpdate.schedule(new
-		 * TimerTask() {
-		 * 
-		 * @Override public void run() { try {
-		 * Log.i("taxi","�a marche le truc de clement"); GeoPoint posTaxi =
-		 * req.getPosTaxi(idTaxi); Collection<String> listItem = map.values();
-		 * String [] list = (String[]) listItem.toArray(); list[0] =
-		 * GmapsDirection.getTrajetInfo(posTaxi, data.position).temps;
-		 * notifyAll(); } catch(ParamsException e) { e.printStackTrace(); }
-		 * catch(HttpUrlException e) { e.printStackTrace(); }
-		 * catch(DirectionNotFoundException e) { e.printStackTrace(); }
-		 * catch(DirectionInvalidRequestException e) { e.printStackTrace(); }
-		 * catch(DirectionException e) { e.printStackTrace(); }
-		 * catch(DirectionZeroResultsException e) { e.printStackTrace(); }
-		 * catch(ConnectionException e) { e.printStackTrace(); } } }, 0, 60000);
-		 */
+		
+		handlerTimer.removeCallbacks(updateEstimmation);
+		handlerTimer.postDelayed(updateEstimmation, 500);
 	}
+
+	private Runnable updateEstimmation = new Runnable() {
+		@Override
+		public void run() {
+			Log.i("taxi", "go timer");
+			ClientRequest req = new ClientRequest(SERVER_ADDR);
+			TextView tempsTaxi = (TextView) findViewById(R.id.EstimationTempsTaxi);
+			try {
+				GeoPoint geo = req.getPosTaxi(idTaxi);
+				TrajetInfo infos = GmapsDirection.getTrajetInfo(geo,
+						data.position);
+				tempsTaxi.setText(infos.temps);
+			} catch(ParamsException e) {
+				e.printStackTrace();
+			} catch(HttpUrlException e) {
+				e.printStackTrace();
+			} catch(ConnectionException e) {
+				e.printStackTrace();
+			} catch(DirectionNotFoundException e) {
+				e.printStackTrace();
+			} catch(DirectionInvalidRequestException e) {
+				e.printStackTrace();
+			} catch(DirectionException e) {
+				e.printStackTrace();
+			} catch(DirectionZeroResultsException e) {
+				e.printStackTrace();
+			} catch(OverQueryLimitException e) {
+				tempsTaxi.setText("inconnue");
+			}
+			Message msg = new Message();
+			msg.what = UPDATE_UI;
+			updateUiEvent.sendMessage(msg);
+			handlerTimer.postDelayed(this, UPDATE_TIME);
+		}
+	};
+	private Handler updateUiEvent = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			switch(msg.what) {
+			case UPDATE_UI:
+				break;
+			default:
+				super.handleMessage(msg);
+			}
+		}
+	};
 }
